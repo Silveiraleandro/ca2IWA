27c4897111bd2843f4fa82e57b24de78cc7eb4e5
Leandro Silveira <leovoz@yahoo.com.br>
2020-04-19 22:42:01 +0000

    change the name nba-controller so it works now

--- routes/nba.js
+++ routes/nba.js
@@ -2,10 +2,15 @@ const express  = require('express');
 const router  = express.Router();
 
 // CONTROLLERS
-const applicationController = require('./../controllers/applicationConroller');
-
+const applicationController = require('./../controllers/nba-Conroller');
+//router get and post that will create players and post it
 router
   .route('/')
   .get(applicationController.home)
+  .post(applicationController.createNbaPlayer);
+//router to delete players
+router
+  .route('/delete')
+  .post(applicationController.deleteNbaPlayer);
 
   module.exports = router;
