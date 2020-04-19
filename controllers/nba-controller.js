ce2ff4f45b1d80a2eec2095b3bb4a5d852b1eb1d
Leandro Silveira <leovoz@yahoo.com.br>
2020-04-19 23:20:23 +0000

    separate the generation of HTML from the rest of the JavaScript and handle html. I also separeted the files in different folders. specially the ones that I still do not know if I will use

--- controllers/nba-controller.js
+++ controllers/nba-controller.js
@@ -1,65 +1,22 @@
-var NBA = require('../models/nbaPlayer'),
-express = require('express'),
-router = express.Router();
+const NbaPlayer = require('./../models/nbaPlayer');
 
-router.get('/get/html', function(req, res) {
+exports.home = async (req, res, next) => {
+  const _players = await NbaPlayer.find({});
+  const players = [];
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    //reading the xml files
-    var xml = fs.readFileSync('CA_IWA.xml', 'utf8');
-    var xsl = fs.readFileSync('CA_IWA.xsl', 'utf8');
-    //parsing xml
-    var doc = xmlParse(xml);
-    var stylesheet = xmlParse(xsl);
-    //performing transformation
-    var result = xsltProcess(doc, stylesheet);
-    //returning it in String format
-    res.end(result.toString());
+  _players.map((p) => {
+    players.push(p.toJSON());
+  });
+  res.render('home', { players: players });
+};
 
+exports.createNbaPlayer = async (req, res, next) => {
+  const player = await NbaPlayer.create(req.body);
+  res.redirect('/');
+};
 
-});
-// POST request to add to JSON & XML files
-router.post('/post/json', function(req, res) {
+exports.deleteNbaPlayer = async (req, res, next) => {
+  await NbaPlayer.findByIdAndDelete(req.body.id);
 
-  // reading in a JSON file and adding to it & convert to XML
-  function appendJSON(obj) {
-    //console.log(obj);
-    // reading in XML file, converting it to JSON, adding a new object and writing back to XML file
-    xmlFileToJs('CA_IWA.xml', function(err, result) {
-      if (err) throw (err);
-      result.NBA.section[obj.sec_n].entree.push({'player': obj.player, 'value': obj.value, 'team': obj.team});
-        //from JSON back to xml
-        jsToXmlFile('CA_IWA.xml', result, function(err) {
-        if (err) console.log(err);
-      })
-    })
-  }
-
-  // Call appendJSON function and pass in body of the current POST request
-  appendJSON(req.body);
-
-  // browser goes back to the page of the POST
-  res.redirect('back');
-
-});
-// POST request to add to JSON & XML files
-router.post('/post/delete', function(req, res) {
-
-  // Function to read in a JSON file, add to it & convert to XML
-  function deleteJSON(obj) {
-    // reading in XML file, converting it to JSON, deleting the required object and writing back to XML file
-    xmlFileToJs('CA_IWA.xml', function(err, result) {
-      if (err) throw (err);
-      //deleting an object based on the position of the section and position of the entree
-      delete result.NBA.section[obj.section].entree[obj.entree];
-      //from JSON back our XML file
-      jsToXmlFile('CA_IWA.xml', result, function(err) {
-        if (err) console.log(err);
-      })
-    })
-  }
-
-  // body of the current POST request
-  deleteJSON(req.body);
-
-});
+  res.redirect('/');
+};
