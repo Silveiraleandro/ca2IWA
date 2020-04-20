a6cfce2a00ab819d7ca6493dc9985e880d2d49e7
Leandro Silveira <leovoz@yahoo.com.br>
2020-04-20 22:59:14 +0000

    just to save

--- controllers/nba-controller.js
+++ controllers/nba-controller.js
@@ -3,18 +3,18 @@ const NbaPlayer = require('./../models/nbaPlayer');
 exports.home = async (req, res, next) => {
   const _players = await NbaPlayer.find({})
   const players = [];
-
+//parsing the mongo objects that are in the array to json 
   _players.map((p) => {
     players.push(p.toJSON());
   });
   res.render('home', { players: players });
 };
-
+//creating an object player and redirecting the index page
 exports.createNbaPlayer = async (req, res, next) => {
   const player = await NbaPlayer.create(req.body);
   res.redirect('/');
 };
-
+//deleting an object player and redirecting the index page
 exports.deleteNbaPlayer = async (req, res, next) => {
   await NbaPlayer.findByIdAndDelete(req.body.id);
 
