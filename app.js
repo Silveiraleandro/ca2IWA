554db4a1d69078e3df151c6e5f3a3812a9d4a264
Leandro Silveira <leovoz@yahoo.com.br>
2020-04-22 19:27:08 +0000

    update method

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const applicationRoutes = require('./routes/nba');
 
 
@@ -10,8 +11,7 @@ const morgan = require('morgan');
 // ROUTER IMPORTS
 
 const app = express();
-app.use(express.static('public/assets/css'));
-app.use(express.static('public/assets/boostrap.min.js'));
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'public/views'));
@@ -24,7 +24,16 @@ if(process.env.NODE_ENV === 'development') {
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
+// override with different headers; last one takes precedence
+app.use(bodyParser.urlencoded())
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        const method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
 // ROUTES
 // Application Routes
 app.use('/', applicationRoutes);
